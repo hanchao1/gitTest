b3a79a34d7c8d90b4dd52adf2829846949ca439e
han <hanchao1710@163.com>
2022-12-10 18:20:57 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author hanchao
+ * @Date 2022/12/10 18:16
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
